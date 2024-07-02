a338821c4f00d3f314f7c644f193a6f864b5d092
Stephano <nafetsgary@outlook.es>
2024-07-02 17:26:47 -0500

    Pregunta 3 terminada - Cors activado

--- src/main/java/pe/edu/cibertec/dsw2_t3_aguerostephano/Dsw2T3AgueroStephanoApplication.java
+++ src/main/java/pe/edu/cibertec/dsw2_t3_aguerostephano/Dsw2T3AgueroStephanoApplication.java
@@ -2,12 +2,27 @@ package pe.edu.cibertec.dsw2_t3_aguerostephano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Dsw2T3AgueroStephanoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Dsw2T3AgueroStephanoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer configGlobalCors(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("api/v1/**")
+                        .allowedMethods("GET", "POST", "PUT")
+                        .allowedOrigins("https://www.cibertec.edu.pe");
+            }
+        };
     }
 
 }
