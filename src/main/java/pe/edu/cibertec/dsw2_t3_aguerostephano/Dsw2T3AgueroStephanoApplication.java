6997b8c579690c07745e420e28afca1686541a70
Stephano <nafetsgary@outlook.es>
2024-07-02 17:01:36 -0500

    iniciando proyecto

---
+++ src/main/java/pe/edu/cibertec/dsw2_t3_aguerostephano/Dsw2T3AgueroStephanoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dsw2_t3_aguerostephano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw2T3AgueroStephanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dsw2T3AgueroStephanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/dsw2_t3_aguerostephano/Dsw2T3AgueroStephanoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dsw2_t3_aguerostephano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw2T3AgueroStephanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
