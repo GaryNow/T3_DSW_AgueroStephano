0e84987f9f46861044f8f31d86f5a2ef25d9fdd2
Stephano <nafetsgary@outlook.es>
2024-07-02 17:24:58 -0500

    Pregunta 3 - Mapeo terminada

--- src/main/java/pe/edu/cibertec/dsw2_t3_aguerostephano/controller/Pregunta3Controller.java
+++ src/main/java/pe/edu/cibertec/dsw2_t3_aguerostephano/controller/Pregunta3Controller.java
@@ -7,7 +7,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pe.edu.cibertec.dsw2_t3_aguerostephano.model.dto.ArchivoDto;
+import pe.edu.cibertec.dsw2_t3_aguerostephano.model.dto.DtoEntity;
 import pe.edu.cibertec.dsw2_t3_aguerostephano.service.FileService;
+import pe.edu.cibertec.dsw2_t3_aguerostephano.service.IPublicacionService;
+import pe.edu.cibertec.dsw2_t3_aguerostephano.util.DtoUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,8 @@ import java.util.stream.Collectors;
 @RequestMapping("api/pregunta3")
 public class Pregunta3Controller {
 
+    private IPublicacionService iPublicacionService;
+
     private FileService fileService;
 
     @PostMapping("/filesimages")
@@ -27,5 +32,16 @@ public class Pregunta3Controller {
         return new ResponseEntity<>(ArchivoDto.builder().mensaje("Archivos subidos correctamente").build(), HttpStatus.OK);
     }
 
-
+    @GetMapping("/pubs-dto")
+    @PreAuthorize("hasRole('COORDINADOR')")
+    public ResponseEntity<List<DtoEntity>> listaPublicacionDto(){
+        List<DtoEntity> publicacionDtoList = new ArrayList<>();
+        publicacionDtoList = iPublicacionService.listarPublicaciones()
+                .stream()
+                .map(x -> new DtoUtil().convertirPublicacionAPublicacionDto(x))
+                .collect(Collectors.toList());
+        if(publicacionDtoList.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(publicacionDtoList, HttpStatus.OK);
+    }
 }
